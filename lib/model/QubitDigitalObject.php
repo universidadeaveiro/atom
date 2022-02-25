14feba08a8e77564df0cb4df0f4551d8566eadbc
Rafael Direito <rafael.neves.direito@ua.pt>
2022-02-25 19:30:35 +0000

    ffmpeg tests

--- lib/model/QubitDigitalObject.php
+++ lib/model/QubitDigitalObject.php
@@ -2982,7 +2982,7 @@ class QubitDigitalObject extends BaseDigitalObject
       return false;
     }
 
-    $command = 'ffmpeg -y -i '.$originalPath.' -ar 44100 -c:v libx264 -pix_fmt yuv420p -c:a aac -movflags +faststart '.$newPath.' 2>&1';
+    $command = 'ffmpeg -threads 2 -y -i '.$originalPath.' -ar 44100 -c:v libx264 -pix_fmt yuv420p -c:a aac -movflags +faststart '.$newPath.' 2>&1';
     exec($command, $output, $status);
 
     chmod($newPath, 0644);
